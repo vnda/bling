38f8141854181de4f34c8c91b8925a949373311e
Francisco DC <fdc@razaoinfo.com.br>
2013-07-23 20:36:19 -0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VndaBling::Application.config.secret_token = '8c5ed27535f4d82e1ba1d7514ba8f67e55ad90df440ced631ad5952fc55a20703884453a989ab43063ec80efeae70b4a0709ac647b9e1aef2ecdec2c51dd598b'
