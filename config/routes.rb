a6e723e40f7bcb3a35c0be977dbc85473be4e3de
Francisco DC <fdc@razaoinfo.com.br>
2013-07-24 14:53:19 -0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 VndaBling::Application.routes.draw do
-  root :to => 'bling#index'
+  root :to => 'shops#index'
+
+  resources :shops
+
   post '/bling', :to => 'bling#create'
+
+  get "/login", :to => "login#new", :as => :login
+  post "/login", :to => "login#create"
+  delete "/logout", :to => "login#destroy", :as => :logout
+  get "/password", :to => "login#renew_password", :as => :renew_password
+  post "/password", :to => "login#set_password"
 end
