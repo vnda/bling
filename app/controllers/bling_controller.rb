990f349445d9d9a3eb19a4537825f3a8402db60a
Francisco DC <fdc@razaoinfo.com.br>
2013-07-24 14:54:36 -0300

    escape authentication on Bling controller

--- app/controllers/bling_controller.rb
+++ app/controllers/bling_controller.rb
@@ -2,6 +2,8 @@
 require 'bling'
 
 class BlingController < ApplicationController
+  skip_filter :authenticate!, :only => [:create]
+
   def create
     order = MultiJson.load(params[:order] || '{}')
     bling = Bling.new('095ce0af267fce959dce5cdba481d8cabf891d8d')
