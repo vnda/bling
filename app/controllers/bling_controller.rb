b5e24614a072a71d074b998dc40879aab5a4d812
Francisco DC <fdc@razaoinfo.com.br>
2013-07-24 23:46:03 -0300

    raise errors instead of returning

--- app/controllers/bling_controller.rb
+++ app/controllers/bling_controller.rb
@@ -5,17 +5,16 @@ class BlingController < ApplicationController
   skip_filter :authenticate!, :only => [:create]
 
   def create
-    if params[:token].blank?
-      render :json => {:error => 'token missing'}
-    else
-      access_token = params[:token].gsub(/[^0-9A-Za-z]/, '')
-      if shop = Shop.where(:access_token => access_token).first
-        order = MultiJson.load(request.body.read || '{}')
-        bling = Bling.new(shop.bling_key)
-        render :json => bling.send('order', order)
-      else
-        render :json => {:error => 'invalid token'}
-      end
-    end
+    raise "token missing" if params[:token].blank?
+    access_token = params[:token].gsub(/[^0-9A-Za-z]/, '')
+    shop = Shop.where(:access_token => access_token).first
+    raise "token not found" unless shop
+
+    body = request.body.read
+    raise "request body is empty" if body.blank?
+    order = MultiJson.load(body)
+
+    bling = Bling.new(shop.bling_key)
+    render :json => bling.send('order', order)
   end
 end
--- lib/bling.rb
+++ lib/bling.rb
@@ -11,13 +11,12 @@ class Bling
       xml = generate_xml(order)
       response = send_to_bling(type, xml)
     else
-      response = {:status => 'error', :message => 'params missing'}
+      raise 'params missing'
     end
     response
   end
 
   def send_to_bling(type, xml)
-    report = {:status => 'error'}
     url = "http://www.bling.com.br"
     post_url = (type == 'nfe' ? '/recepcao.nfe.php' : '/recepcao.pedido.php')
     post_params = {
@@ -31,19 +30,9 @@ class Bling
       builder.adapter :net_http
     end
 
-    begin
-      response = connection.post post_url, post_params
-    rescue Faraday::Error::ConnectionFailed
-      report[:message] = 'connection failed'
-    else
-      if response.body == 'OK'
-        report[:status] = 'success'
-      else
-        report[:message] = response.body
-      end
-    end
-
-    return report
+    response = connection.post post_url, post_params
+    raise response.body unless response.body == 'OK'
+    true
   end
 
   def generate_xml(order)
@@ -54,38 +43,31 @@ class Bling
         xml.tag!("nome", [order["first_name"], order["last_name"]].join(' '))
         xml.tag!("tipoPessoa", (order["cpf"].present? ? 'F' : 'J') )
         xml.tag!("cpf_cnpj", order["cpf"] || order["cnpj"])
-        xml.tag!("ie_rg", order["rg"])  #need
+        xml.tag!("ie_rg", order["rg"])
         xml.tag!("endereco", order["street_name"])
         xml.tag!("numero", order["street_number"])
-        xml.tag!("complemento", order["complement"]) #op
+        xml.tag!("complemento", order["complement"])
         xml.tag!("bairro", order["neighborhood"])
         xml.tag!("cep", order["zip"])
         xml.tag!("cidade", order["city"])
         xml.tag!("uf", order["state"])
-        xml.tag!("fone", order["state"]) #op
         xml.tag!("email", order["email"])
       end
       xml.tag!("itens") do
         order["items"].each do |item|
           xml.tag!("item") do
-            xml.tag!("codigo", [item["reference"], item["sku"]].join(' - ')) #op
+            xml.tag!("codigo", [item["reference"], item["sku"]].join(' - '))
             xml.tag!("descricao", [item["product_name"], item["variant_name"]].join(' - '))
-            xml.tag!("un", 'un')  #need
+            xml.tag!("un", 'un')
             xml.tag!("qtde", item["quantity"])
             xml.tag!("vlr_unit", item["subtotal"])
-            xml.tag!("tipo", 'P')  #need
-            xml.tag!("peso_bruto", 0) #op  #need
-            xml.tag!("peso_liq", 0) #op  #need
-            xml.tag!("class_fiscal", '9999.99.99') #op  #need
-            xml.tag!("origem", 0) #need
+            xml.tag!("tipo", 'P')
+            xml.tag!("origem", 0)
           end
         end
       end
       xml.tag!("vlr_frete", order["shipping_price"])
-      xml.tag!("vlr_seguro", '') #op
-      xml.tag!("vlr_despesas") #op
       xml.tag!("vlr_desconto", order["discount_price"])
-      xml.tag!("obs", '') #op
     end
     xml.target!.tap { |str| Rails.logger.info(str) }
   end
