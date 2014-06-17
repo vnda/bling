9c09db001d5471c8c9045e304b35afac34b6b5d4
Germano Nicolini <germanogmn1@gmail.com>
2014-06-17 17:14:52 -0300

    upgrade bootstrap to 3.1.1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,16 @@
-#encoding: utf-8
 module ApplicationHelper
+  def icon_tag(icon_sym, options = {})
+    klass = "glyphicon glyphicon-#{icon_sym.to_s.gsub(?_, ?-)}"
+    content_tag(:i, '', options.merge(class: klass))
+  end
+
   def delete_action(resource, allowed = true, options = {})
     if allowed
-      link_to("&nbsp;".html_safe, resource, :method => :delete, :data => { :confirm => "Tem certeza que deseja excluir este item?" }, :class => "icon-remove", :style => "text-decoration:none;")
+      confirm = 'Tem certeza que deseja excluir este item?'
+      link_to(icon_tag(:remove), resource, method: :delete, data: { confirm: confirm })
     else
-      options[:tooltip] ||= "Este item não pode ser excluído pois possui outros itens relacionados"
-      content_tag(:i, "&nbsp;".html_safe, :class => "icon-remove", :style => "opacity:0.3;", :rel => "tooltip", :title => options[:tooltip])
+      options[:tooltip] ||= 'Este item não pode ser excluído pois possui outros itens relacionados'
+      icon_tag(:remove, style: 'opacity: 0.3', title: options[:tooltip], data: { toggle: 'tooltip' })
     end
   end
 end
